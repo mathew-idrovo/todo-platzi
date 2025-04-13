523bbe96a311f00202c7e3b400f8f5fd7fe749dc
Mathew <mateoicecua2000@gmail.com>
2025-04-12 23:16:53 -0500

    feat(modal): add modal structure and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { motion } from 'motion/react'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {' '}
+      <motion.header animate={{ rotate: 360 }}>
+        <div>
+          <h1 className="text-2xl">taskify</h1>
+          <h1>Gestiona tus tareas diarias de forma sencilla y rápida.</h1>
+        </div>
+      </motion.header>
+      <motion.main>
+        <Button variant="default" size="lg" className="bg-blue-500">
+          Nueva tarea
+        </Button>
+      </motion.main>
     </>
   )
 }
