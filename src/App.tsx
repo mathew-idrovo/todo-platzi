9114dc5fd449adc3f217dda353485fd32597a2ed
Mathew <mateoicecua2000@gmail.com>
2025-04-13 00:09:01 -0500

    feat(modal): add modal effects and events

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { motion } from 'motion/react'
 import './App.css'
 import { Button } from './components/ui/button'
+import { ModalTask } from './components/ModalTask'
+import { useState } from 'react'
+import { Task } from './types/task'
 
 function App() {
+  const [currentTask, setCurrentTask] = useState<Task | null>(null)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const openCreateModal = () => {
+    setCurrentTask(null)
+    setIsModalOpen(true)
+  }
   return (
     <>
       {' '}
@@ -13,9 +22,20 @@ function App() {
         </div>
       </motion.header>
       <motion.main>
-        <Button variant="default" size="lg" className="bg-blue-500">
+        <Button
+          variant="default"
+          size="lg"
+          className="bg-blue-500"
+          onClick={openCreateModal}
+        >
           Nueva tarea
         </Button>
+        <ModalTask
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          onSave={() => {}}
+          task={currentTask}
+        />
       </motion.main>
     </>
   )
