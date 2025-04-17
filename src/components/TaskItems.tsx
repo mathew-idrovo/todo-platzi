6559b31d0ee6836a9e47cf6d4e6c4ad93dad43d2
Mathew <mateoicecua2000@gmail.com>
2025-04-16 19:35:06 -0500

    feat(gh-pages): add script for deploy

--- src/components/TaskItems.tsx
+++ src/components/TaskItems.tsx
@@ -51,10 +51,10 @@ export const TaskItems = ({
     }),
   }
 
-  const buttonVariants = {
-    initial: { opacity: 0, scale: 0.8 },
-    animate: { opacity: 1, scale: 1, transition: { duration: 0.2 } },
-  }
+  // const buttonVariants = {
+  //   initial: { opacity: 0, scale: 0.8 },
+  //   animate: { opacity: 1, scale: 1, transition: { duration: 0.2 } },
+  // }
   return (
     <motion.div
       ref={cardRef}
